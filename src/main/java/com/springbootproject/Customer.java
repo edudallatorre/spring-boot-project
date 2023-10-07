aa39b38d105aaf9c5a0e500c281402fb72251673
edudallatorre <eduardodallatorre.io@gmail.com>
2023-10-07 14:56:27 +0100

    Updating project and fixing bugs

--- src/main/java/com/springbootproject/Customer.java
+++ src/main/java/com/springbootproject/Customer.java
@@ -1,18 +1,31 @@
 package com.springbootproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
 
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
     private Integer age;
 
-    public Customer(Integer id,
-                    String name,
-                    String email,
-                    Integer age) {
+    public Customer(Integer id, String name, String email, Integer age) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -20,40 +33,9 @@ public class Customer {
     }
 
     public Customer() {
-
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+    // Getters e setters
 
     @Override
     public boolean equals(Object o) {
@@ -78,5 +60,3 @@ public class Customer {
                 '}';
     }
 }
-
-
--- src/main/java/com/springbootproject/Main.java
+++ src/main/java/com/springbootproject/Main.java
@@ -3,67 +3,28 @@ package com.springbootproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
+
 public class Main {
+    private final CustomerRepository customerRepository;
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        GreetResponse response = new GreetResponse(
-                "Hello",
-                List.of("Java", "PHP", "JavaScript"),
-                new Person("Eduardo", "Developer", 30_000)
-        );
-        return response;
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
-
-    record Person(String name, String role, double savings) { }
-
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ) {
-    }
-
-//    class GreetResponse {
-//        private final String greeet;
-//
-//        GreetResponse(String greeet) {
-//            this.greeet = greeet;
-//        }
-//
-//        public String getGreeet() {
-//            return greeet;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greeet='" + greeet + '\'' +
-//                    '}';
-//        }
-//
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            GreetResponse that = (GreetResponse) o;
-//            return Objects.equals(greeet, that.greeet);
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return Objects.hash(greeet);
-//        }
-//    }
-
 }
